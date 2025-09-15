73cf169099c768a5ebb74ff10abc95dd64d6f910
Klaaktu <52594481+Klaaktu@users.noreply.github.com>
2025-09-16 06:25:34 +1000

    Refactor and update
    Removed PolygonData abstraction / wrapper. Didn't find too much use for other fields, and it's making me write wrappers for everything.

--- src/instance.rs
+++ src/instance.rs
@@ -1,13 +1,18 @@
-use crate::{Coord2D, polygon::PolygonData};
-use geo::{Area, IsConvex, LineString};
+use crate::Coord2D;
+use geo::{
+    Area, BooleanOps, Coord, CoordinatePosition, IsConvex, LineString, MultiPolygon, Polygon,
+    coordinate_position::CoordPos, unary_union,
+};
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, vec};
 use wasm_bindgen::prelude::*;
 
+static S1: &str = "IDs not found in database! Caller may contain bugs.";
+
 #[wasm_bindgen]
 #[derive(Serialize, Deserialize)]
 pub struct Instance {
-    data: HashMap<u32, PolygonData>,
+    data: HashMap<u32, Polygon>,
     counter: u32, // Used for hash map key, not actual count of items!
 }
 
@@ -27,16 +32,21 @@ impl Instance {
         }
     }
 
-    pub fn add_polygon(&mut self, points: Vec<Coord2D>, color: u32) -> Result<u32, String> {
-        let mut ext_line = LineString::from(points);
-        ext_line.close();
-        if !ext_line.is_convex() {
-            return Err("Shape is not convex!".into());
-        }
+    pub fn add_polygon(&mut self, points: Vec<Coord2D>) -> Result<u32, String> {
+        let p = Instance::vec_to_convex_poly(points)?;
         self.counter += 1;
-        let p = PolygonData::new(ext_line, color, self.counter);
         self.data.insert(self.counter, p);
         Ok(self.counter)
+    }
+
+    pub fn mod_polygon(&mut self, id: u32, points: Vec<Coord2D>) -> Result<(), String> {
+        let p = Instance::vec_to_convex_poly(points)?;
+        self.data.insert(id, p);
+        Ok(())
+    }
+
+    pub fn del_polygon(&mut self, id: u32) -> Result<(), String> {
+        self.data.remove(&id).ok_or(S1.into()).map(|_| ())
     }
 
     // Some fail conditions:
@@ -49,11 +59,11 @@ impl Instance {
         let mut polygons2 = polygons.clone().peekable();
 
         if polygons2.peek().is_none() {
-            return Err("IDs not found in database! Caller may contain bugs.".into());
+            return Err(S1.into());
         }
 
-        let intersection = PolygonData::unary_intersection(polygons).unsigned_area();
-        let union = PolygonData::unary_union(polygons2).unsigned_area();
+        let intersection = Instance::unary_intersection(polygons).unsigned_area();
+        let union = unary_union(polygons2).unsigned_area();
 
         if union == 0.0 {
             return Err("Division by 0! Bad polygon area.".into());
@@ -62,11 +72,20 @@ impl Instance {
         Ok(intersection / union)
     }
 
+    pub fn polygons_under_coord(&self, c: Coord2D) -> Vec<u32> {
+        let geo_coord: Coord = c.into();
+        self.data
+            .iter()
+            .filter(|(_, poly)| poly.coordinate_position(&geo_coord) != CoordPos::Outside)
+            .map(|(&id, _)| id)
+            .collect()
+    }
+
     // Not to be used in IoU
     // This assumes the intersection is always 1 polygon
     pub fn intersection(&self, ids: Vec<u32>) -> Result<Vec<Coord2D>, String> {
         let polygon_data = self.ids_to_polygons(&ids)?;
-        let i = match PolygonData::unary_intersection(polygon_data).iter().next() {
+        let i = match Instance::unary_intersection(polygon_data).iter().next() {
             Some(p) => p.exterior().coords().map(|c| (*c).into()).collect(),
             None => vec![],
         };
@@ -80,21 +99,31 @@ impl Instance {
         }
     }
 
-    pub fn coord_in_polygon(&self, id: u32, c: Coord2D) -> Result<bool, String> {
-        self.data
-            .get(&id)
-            .ok_or("ID does not correspond to a known polygon!".into())
-            .map(|p| p.coord_in_polygon(c))
+    fn vec_to_convex_poly(points: Vec<Coord2D>) -> Result<Polygon, String> {
+        let mut ext_line = LineString::from(points);
+        ext_line.close();
+        if !ext_line.is_convex() {
+            return Err("Shape is not convex!".into());
+        }
+        Ok(Polygon::new(ext_line, vec![]))
     }
 
     fn ids_to_polygons(
         &self,
         ids: &Vec<u32>,
-    ) -> Result<impl Iterator<Item = &PolygonData> + Clone, String> {
+    ) -> Result<impl Iterator<Item = &Polygon> + Clone, String> {
         if ids.is_empty() {
             return Err("Empty list of IDs!".into());
         };
         Ok(ids.iter().filter_map(|id| self.data.get(id)))
+    }
+
+    fn unary_intersection<'a>(mut polygons: impl Iterator<Item = &'a Polygon>) -> MultiPolygon {
+        let intersection = match polygons.next() {
+            None => return MultiPolygon::new(vec![]),
+            Some(pointer) => MultiPolygon::new(vec![pointer.clone()]),
+        };
+        polygons.fold(intersection, |acc, p| acc.intersection(p))
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 mod api;
 mod instance;
-mod polygon;
 
 pub use api::*;
 pub use instance::TextFormat;
